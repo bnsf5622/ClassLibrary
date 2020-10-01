6afceec5f52a0bf74b74357f84fd635d8bad1f83
chuck francis <chuck_francis10@yahoo.com>
2020-10-01 17:22:27 -0400

    Add project files.

---
+++ UserInput.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class UserInput
+    {
+        public int GetIntFromUser(string prompt)
+        {
+            string input;
+            int output;
+
+            while (true)
+            {
+                Console.Write(prompt);
+                input = Console.ReadLine();
+
+                if (int.TryParse(input, out output))
+                    return output;
+
+                Console.Write("That was not a valid integer... press any key to continue");
+                Console.ReadKey();
+            }
+        }
+    }
+}
